58cebe47389947a93861b504661243a18d09fb0f
KaYo <kayoczl@163.com>
2023-02-21 20:51:43 +0800

    6-4 【路由架构】基础路由系统

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path='/signIn' element={<h1>登录页面</h1>} />
+          <Route path='*' element={<h1>404 NOT FOUND 页面不存在</h1>} ></Route>
         </Routes>
       </BrowserRouter>
     </div>
