aa9c4ebab880cb496b80d3a7ab3b2884f19e4762
KaYo <kayoczl@163.com>
2023-03-24 05:48:05 +0800

    11-3【UI搭建】购物车页面初始化

--- src/pages/shoppingCart/ShoppingCart.tsx
+++ src/pages/shoppingCart/ShoppingCart.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import styles from './index.module.less'
-import { Layout, Typography } from 'antd';
-import { useTranslation } from 'react-i18next'
+import { MainLayout } from "../../layouts";
+import { Row, Col, Affix } from 'antd'
+import { ProductList, PaymentCard } from "../../components";
 
 export const ShoppingCart: React.FC = () => {
   
 
   return (
-    <div>
-      购物车
-    </div>
+    <MainLayout>
+      <Row>
+        {/* 购物车清单 */}
+        <Col span={16}>
+          <div className={styles["product-list-container"]}>
+            {/* <ProductList /> */}
+          </div>
+        </Col>
+        {/* 支付卡组件 */}
+        <Col span={8}>
+          <Affix>
+            <div className={styles["payment-card-container"]}>
+              {/* <PaymentCard /> */}
+            </div>
+          </Affix>
+        </Col>
+      </Row>
+    </MainLayout>
   )
 }
